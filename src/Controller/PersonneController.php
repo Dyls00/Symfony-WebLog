9daf8444e8702cf9ccb1effa28f7740cfa1192b6
johane <nsangouakandajohane@gmail.com>
2024-06-28 06:24:43 +0200

    update

--- src/Controller/PersonneController.php
+++ src/Controller/PersonneController.php
@@ -2,17 +2,30 @@
 
 namespace App\Controller;
 
+use App\Entity\Personne;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class PersonneController extends AbstractController
 {
-    #[Route('/personne', name: 'app_personne')]
-    public function index(): Response
+    #[Route('/personne/add', name: 'app_personne')]
+    public function addPersonne( ManagerRegistry $doctrine): Response
     {
-        return $this->render('personne/index.html.twig', [
-            'controller_name' => 'PersonneController',
+        $entityManager = $doctrine->getManager();
+        $personne = new Personne();
+        $personne -> setFirstname('johane');
+        $personne -> setName('dyls');
+        $personne -> setAge(26);
+
+        // Ajouter l'operation d'insertion de la personne
+        $entityManager -> persist($personne);
+
+        // executer l'operation
+
+        return $this->render('personne/details.html.twig', [
+            'personne' => '$personne',
         ]);
     }
 }
